04ba1a428af8a1aa4df196fd73f4d74c2abc254d
quantosapplications <info@quantos.it>
2021-05-04 14:56:39 +0200

    gcp apollo logger

--- src/utils/gcp_apollo_logger.ts
+++ src/utils/gcp_apollo_logger.ts
@@ -2,7 +2,7 @@ import { GraphQLError, GraphQLFormattedError } from 'graphql';
 import { ApolloError } from 'apollo-server-core';
 
 export const GcpApolloLogger = {
-    formatError: (error: GraphQLError): GraphQLFormattedError<Record<string, any>> | undefined => {
+    formatError: (error: GraphQLError): GraphQLFormattedError<Record<string, any>> => {
         if (error.originalError instanceof ApolloError) {
             console.warn(error.message, error.originalError); // faccio un warn perchè Apollo error corrisponde a un 4xx error (utente)
             return error;
